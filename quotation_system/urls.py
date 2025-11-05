f0c6d4b9b6e377f6aa604fbce1c4aba640f402ee
leoBravo <leo.bravo.rain@gmail.com>
2025-11-05 12:26:03 -0300

    refactor(users): move login and refresh token for users app and custom views and urls

--- quotation_system/urls.py
+++ quotation_system/urls.py
@@ -15,14 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # authentication
-    # TODO: craete separate app for authentication
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # user app
+    path('api/users/', include('quotation_system.users.urls')),
 ]
--- quotation_system/users/views.py
+++ quotation_system/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# Create your views here.
+# TODO: add typing?
+class LoginView(TokenObtainPairView):
+    """
+    View for user login.
+    Returns access and refresh tokens.
+    """
+    pass
+
+class RefreshTokenView(TokenRefreshView):
+    """
+    View for refreshing access tokens.
+    """
+    pass
