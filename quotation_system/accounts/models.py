8300aad647f86f8265ca77075a76a0474abc250d
leoBravo <leo.bravo.rain@gmail.com>
2025-11-06 14:40:53 -0300

    feat(accouts): create account model. Integrate app into project

--- quotation_system/accounts/models.py
+++ quotation_system/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Account(models.Model):
+    account_number = models.PositiveIntegerField(unique=True, editable=False)
+    currency = models.CharField(max_length=3)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.account_number} - {self.currency} - {self.balance}"
+    
