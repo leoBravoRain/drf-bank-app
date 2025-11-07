311d4ac68aacc5d83c98216b8ebd62d8668e933d
leoBravo <leo.bravo.rain@gmail.com>
2025-11-07 15:05:48 -0300

    test(accounts): implement pytest. Change account unit test to pytest

--- quotation_system/accounts/tests/test_views_unit.py
+++ quotation_system/accounts/tests/test_views_unit.py
@@ -1,59 +1,58 @@
-from django.test import SimpleTestCase
+import pytest
 from rest_framework.test import APIRequestFactory
 from unittest.mock import Mock, create_autospec
 from quotation_system.accounts.views import AccountListView
 from quotation_system.accounts.serializers import AccountSerializer
 from unittest.mock import patch, MagicMock
-
-
-class AccountListViewTestCase(SimpleTestCase):
     
-    def setUp(self):
-        # create a request object to pass to the view (instead of using the client who makes the http reuqest)
-        self.factory = APIRequestFactory()
-        self.user = Mock()
-        self.user.id = 1
-        self.view = AccountListView()
-        
-    def test_perform_create_sets_user_and_zero_balance(self):
-        # arrange
-        
-        # creates a DRF Request object representing a POST to path /. It’s not sent over HTTP — it’s just an object.
-        request = self.factory.post("/")
-        
-        # attaches the fake user to the request. 
-        # The view’s perform_create uses self.request.user, so we must supply it.
-        request.user = self.user
-        
-        # assigns the fake request to the view instance so the view method reads it normally.
-        self.view.request = request
+# fixtures
+@pytest.fixture
+def api_factory():
+    """Provides a DRF APIRequestFactory instance."""
+    return APIRequestFactory()
 
-        # create a serializer object instead of using the real serializer
-        # creates a mock that enforces the interface of AccountSerializer
-        serializer = create_autospec(AccountSerializer, instance=True)
+@pytest.fixture
+def user():
+    """Provides a fake user object."""
+    user = MagicMock()
+    user.id = 1
+    return user
 
-        # act
-        
-        # This calls the method under test
-        self.view.perform_create(serializer)
+@pytest.fixture
+def view():
+    """Provides a AccountListView instance."""
+    return AccountListView()
 
-        # assert
-        # This assertion checks that the mocked serializer’s save method was called exactly once and with the exact keyword arguments user=self.user and balance=0
-        serializer.save.assert_called_once()
-        serializer.save.assert_called_once_with(user=self.user, balance=0)
+def test_perform_create_sets_user_and_zero_balance(api_factory, user, view):
+    """Test that the perform_create method sets the user and zero balance."""
+    #  arrange
+    request = api_factory.post("/")
+    request.user = user
+    view.request = request
     
-    @patch('quotation_system.accounts.views.Account.objects.filter')
-    def test_get_queryset_returns_accounts_for_the_user(self, mock_filter):
-        # Arrange
-        request = self.factory.get("/")
-        request.user = self.user
-        self.view.request = request
-
-        # Act
-        self.view.get_queryset()
-
-        # Assert
-        mock_filter.assert_called_once()
-        mock_filter.assert_called_once_with(user=self.user)
+    # create a serializer object instead of using the real serializer
+    # creates a mock that enforces the interface of AccountSerializer
+    serializer = create_autospec(AccountSerializer, instance=True)
     
+    # act
+    view.perform_create(serializer)
+    
+    # assertj
+    serializer.save.assert_called_once()
+    serializer.save.assert_called_once_with(user=user, balance=0)
+    
+def test_get_queryset_returns_accounts_for_the_user(api_factory, user, view, mocker):
+    # Arrange
+    mock_filter = mocker.patch('quotation_system.accounts.views.Account.objects.filter')
+    
+    request = api_factory.get("/")
+    request.user = user
+    view.request = request
+    
+    # Act
+    view.get_queryset()
+    
+    # Assert
+    mock_filter.assert_called_once()
+    mock_filter.assert_called_once_with(user=user)
         
