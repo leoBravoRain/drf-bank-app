0d5f12272477998701aa42672efc6577182d15d0
leoBravo <leo.bravo.rain@gmail.com>
2025-11-09 13:30:27 -0300

    test(trsx): add more unit test for when no engouh balance and when no related_account was provided

--- quotation_system/transactions/tests/test_view_unit.py
+++ quotation_system/transactions/tests/test_view_unit.py
@@ -207,6 +207,10 @@ def test_perform_create_transfer_sets_user_and_update_account_balance(api_factor
     # assert serializer was called one time
     assert serializer.save.call_count == 1
 
+    # assert account.save() method was called once for each account
+    assert sender_account.save.call_count == 1
+    assert receiver_account.save.call_count == 1
+    
     new_sender_balance = initial_sender_balance - data['amount']
     
     # assert balances updating
@@ -218,8 +222,111 @@ def test_perform_create_transfer_sets_user_and_update_account_balance(api_factor
     assert serializer.validated_data['previous_balance'] == initial_sender_balance
     assert serializer.validated_data['new_balance'] == new_sender_balance
     
+@pytest.mark.unit
+def test_perform_create_transfer_without_enough_balance(api_factory, user, view, mocker):
     
+    # arrange
+    transaction_amount = 50
+    initial_sender_balance = 0
+    initial_receiver_balance = 0
+    
+    data = {
+        "transaction_type": Transaction.TRANSACTION_TYPES[2][0],
+        "account": 1,
+        "amount": transaction_amount,
+        "currency": "USD",
+        "related_account": 2
+    }
+    # create request obejct
+    request =api_factory.post(
+        "/",
+        data,
+        format="json",
+    )
+    view.request = view.initialize_request(request)
+    view.request.user = user
+    
+     # define mock account
+    sender_account = MagicMock(spec=Account, balance = initial_sender_balance, id = 1)
+    receiver_account = MagicMock(spec=Account, balance = initial_receiver_balance, id = 2)
+    
+    # mock get account
+    # this shoudl be called only once
+    mock_get = mocker.patch(
+        "quotation_system.transactions.views.Account.objects.get",
+        return_value=sender_account
+    )
 
+    # create serializer object
+    serializer = create_autospec(TransactionSerializer, instance=True)
+    serializer.validated_data = {
+        "amount": data['amount'],
+    }
+    
+    # act
+    with pytest.raises(serializers.ValidationError) as e:
+        view.perform_create(serializer)
+    
+    # assert error message
+    assert e.value.detail[0] == "Insufficient balance"
+    
+    # assert save calls
+    assert serializer.save.call_count == 0
+    assert sender_account.save.call_count == 0
+    assert receiver_account.save.call_count == 0
+    
+@pytest.mark.unit
+def test_perform_create_transfer_without_related_account(api_factory, user, view, mocker):
+    
+    # arrange
+    transaction_amount = 50
+    initial_sender_balance = 0
+    initial_receiver_balance = 0
+    
+    data = {
+        "transaction_type": Transaction.TRANSACTION_TYPES[2][0],
+        "account": 1,
+        "amount": transaction_amount,
+        "currency": "USD",
+    }
+    # create request obejct
+    request =api_factory.post(
+        "/",
+        data,
+        format="json",
+    )
+    view.request = view.initialize_request(request)
+    view.request.user = user
+    
+     # define mock account
+    sender_account = MagicMock(spec=Account, balance = initial_sender_balance, id = 1)
+    receiver_account = MagicMock(spec=Account, balance = initial_receiver_balance, id = 2)
+    
+    # mock get account
+    # this shoudl be called only once
+    mock_get = mocker.patch(
+        "quotation_system.transactions.views.Account.objects.get",
+        return_value=sender_account
+    )
+
+    # create serializer object
+    serializer = create_autospec(TransactionSerializer, instance=True)
+    serializer.validated_data = {
+        "amount": data['amount'],
+    }
+    
+    # act
+    with pytest.raises(serializers.ValidationError) as e:
+        view.perform_create(serializer)
+    
+    # assert error message
+    assert e.value.detail[0] == "Related account must be defined"
+    
+    # assert save calls
+    assert serializer.save.call_count == 0
+    assert sender_account.save.call_count == 0
+    assert receiver_account.save.call_count == 0
+    
 @pytest.mark.unit
 def test_perform_create_with_invalid_transaction_type(api_factory, user, view, mocker):
     
