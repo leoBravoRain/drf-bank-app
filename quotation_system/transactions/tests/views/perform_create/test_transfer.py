7b3ddc52eaa579619b601f90ea7d8869abcd90f3
leoBravo <leo.bravo.rain@gmail.com>
2025-11-10 15:50:50 -0300

    test(trx): add test for transfer with different currencies

--- quotation_system/transactions/tests/views/perform_create/test_transfer.py
+++ quotation_system/transactions/tests/views/perform_create/test_transfer.py
@@ -87,6 +87,100 @@ def test_perform_create_transfer_sets_user_and_update_account_balance(
 
 
 @pytest.mark.unit
+def test_perform_create_transfer_with_different_currencies(
+    api_factory, user, view, mocker, mock_atomic
+):
+
+    # arrange
+
+    # trx info
+    transaction_amount = 1
+
+    # sender account info
+    initial_sender_balance = 100
+    sender_currency = "USD"
+
+    # receiver account info
+    initial_receiver_balance = 0
+    receiver_currency = "CLP"
+
+    # currency rate
+    currency_rate = 0.0011
+    data = {
+        "transaction_type": Transaction.TRANSACTION_TYPES[2][0],
+        "account": 1,
+        "amount": transaction_amount,
+        "related_account": 2,
+    }
+    # create request obejct
+    request = api_factory.post(
+        "/",
+        data,
+        format="json",
+    )
+    view.request = view.initialize_request(request)
+    view.request.user = user
+
+    # define mock account
+    sender_account = MagicMock(
+        spec=Account, balance=initial_sender_balance, id=1, currency=sender_currency
+    )
+    receiver_account = MagicMock(
+        spec=Account, balance=initial_receiver_balance, id=2, currency=receiver_currency
+    )
+
+    # mock get account
+    mock_select_for_update = mocker.patch(
+        "quotation_system.transactions.views.Account.objects.select_for_update",
+    )
+    mock_get = mock_select_for_update.return_value.get
+    mock_get.side_effect = [sender_account, receiver_account]
+
+    # create serializer object
+    serializer = create_autospec(TransactionSerializer, instance=True)
+    serializer.validated_data = {
+        "amount": data["amount"],
+    }
+
+    # mock currency query
+    mock_get_currency_rate = mocker.patch(
+        "quotation_system.currencies.utils.CurrencyRate.objects.get"
+    )
+
+    mock_get_currency_rate.return_value.rate = currency_rate
+
+    # act
+    view.perform_create(serializer)
+
+    # Assert - ensure Account.objects.get called twice with correct params
+    expected_calls = [
+        call(user=user, pk=sender_account.id),
+        call(user=user, pk=receiver_account.id),
+    ]
+    mock_get.assert_has_calls(expected_calls)
+
+    # assert serializer was called one time
+    assert serializer.save.call_count == 1
+
+    # assert account.save() method was called once for each account
+    assert sender_account.save.call_count == 1
+    assert receiver_account.save.call_count == 1
+    mock_atomic.assert_called_once()
+
+    coverted_amount = data["amount"] / currency_rate
+    new_sender_balance = initial_sender_balance - data["amount"]
+
+    # assert balances updating
+    assert sender_account.balance == new_sender_balance
+    assert receiver_account.balance == initial_receiver_balance + coverted_amount
+
+    # asserting transactinos sreialiaer balances
+    assert serializer.validated_data["amount"] == data["amount"]
+    assert serializer.validated_data["previous_balance"] == initial_sender_balance
+    assert serializer.validated_data["new_balance"] == new_sender_balance
+
+
+@pytest.mark.unit
 def test_perform_create_transfer_without_enough_balance(
     api_factory, user, view, mocker, mock_atomic
 ):
