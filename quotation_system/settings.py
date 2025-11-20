b4951dadc13db43c1cb119e4e9586035e9f88df5
leoBravo <leo.bravo.rain@gmail.com>
2025-11-20 11:44:55 -0300

    feat(redis): add Redis service to docker-compose and configure Django to use Redis as cache

--- quotation_system/settings.py
+++ quotation_system/settings.py
@@ -186,3 +186,13 @@ structlog.configure(
         JSONRenderer(),
     ],
 )
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",  # Redis DB 1
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
