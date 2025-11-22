23e7148aafe6ffa5436075bdf12604eaad723ba7
leoBravo <leo.bravo.rain@gmail.com>
2025-11-22 18:51:43 -0300

    feat(notifications): implement Dockerfile, FastAPI app, and RabbitMQ email producer

---
+++ quotation_system/notifications/email_producer.py
@@ -0,0 +1,31 @@
+import pika
+from django.conf import settings
+import json
+
+def publish_email(message: dict):
+    credentials = pika.PlainCredentials(
+        settings.RABBITMQ["USER"],
+        settings.RABBITMQ["PASSWORD"]
+    )
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=settings.RABBITMQ["HOST"],
+            port=settings.RABBITMQ["PORT"],
+            credentials=credentials
+        )
+    )
+    channel = connection.channel()
+
+    # IMPORTANT: ensures queue exists!
+    channel.queue_declare(queue=settings.RABBITMQ["QUEUE"], durable=True)
+
+    channel.basic_publish(
+        exchange="",
+        routing_key=settings.RABBITMQ["QUEUE"],
+        body=json.dumps(message).encode("utf-8"),
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # make message persistent
+        )
+    )
+
+    connection.close()
