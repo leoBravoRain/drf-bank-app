a8a91a607116f2f4441c93a070a4d2a78426efbd
leoBravo <leo.bravo.rain@gmail.com>
2025-11-27 11:20:16 -0300

    feat(kafka): integrate Kafka microservice with analytics service and add message handling

--- analytics_service/analytics-service/src/app.controller.ts
+++ analytics_service/analytics-service/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // kafka consumer
+  @MessagePattern('account')
+  handleAnalytics(@Payload() message: any) {
+    console.log('Received from Kakfa', message);
+    return true;
   }
 }
--- analytics_service/analytics-service/src/main.ts
+++ analytics_service/analytics-service/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // add kakfa microservice communication
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['192.168.1.83:9092'],
+      },
+      consumer: {
+        groupId: 'analaytics-service',
+      },
+    },
+  });
+
+  await app.startAllMicroservices();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
